fc6d54f6528084df3f83f17eedb2b0d97e6d5ecf
james-muriithi <muriithijames556@gmail.com>
2019-12-25 23:38:13 +0300

    added docs for demo

--- demo/service-worker.js
+++ demo/service-worker.js
@@ -1,14 +1,12 @@
 self.addEventListener("fetch", function(e) {
-    e.respondWith(caches.open("cachev1").then(function(s) {
+    e.respondWith(caches.open("cachev2").then(function(s) {
         return s.match(e.request).then(function(t) {
             // console.log("cache request: " + e.request.url);
             var n = fetch(e.request).then(function(t) {
                 return console.log("fetch completed: " + e.request.url, t), t && (console.debug("updated cached page: " + e.request.url, t), "GET" === e.request.method && "basic" === t.type && s.put(e.request, t.clone())), t
             }, function(e) {
                 console.log("Error in fetch()", e), e.waitUntil(caches.open("cachev1").then(function(e) {
-                    return e.addAll(["/", "/index.html", "jquery/jquery.min.js", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
-                        "dist/jquery.jCheckBox.min.js", "images/favicon.ico", , "/service-worker.js", "/site.webmanifest"
-                    ])
+                    return e.addAll(["https://james-muriithi.github.io/jCheckBox/"])
                 }))
             });
             return t || n
